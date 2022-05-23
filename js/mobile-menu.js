914331c433c5ee78e150278ee6b15bacdf7c9549
OleksandrRud <oleksandrrud.ua@gmail.com>
2022-05-23 21:54:08 +0300

    finish

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,6 +3,7 @@
     openMenuBtn: document.querySelector(".mobile__button--open"),
     closeMenuBtn: document.querySelector(".mobile__button--close"),
     menu: document.querySelector(".mobile"),
+    body: document.querySelector(".body"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
@@ -10,5 +11,6 @@
 
   function toggleMenu() {
     refs.menu.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
   }
 })();
